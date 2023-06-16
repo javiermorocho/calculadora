5c692c6663304629d4f2d34717415b7497929d15
Javier Morocho <111466976+javiermorocho@users.noreply.github.com>
2023-06-15 23:41:58 -0500

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -2,15 +2,12 @@ class Calculadora {
     sumar(num1, num2) {
         return num1 + num2;
     }
-
     restar(num1, num2) {
         return num1 - num2;
     }
-
     dividir(num1, num2) {
         return num1 / num2;
     }
-
     multiplicar(num1, num2) {
         return num1 * num2;
     }
